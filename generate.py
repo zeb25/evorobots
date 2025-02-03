4201728555afb62c4002d088c9492b6543880f10
Zoe <zoe.bell@uvm.edu>
2025-02-03 14:35:50 -0500

    Updating branch

--- generate.py
+++ generate.py
@@ -1,4 +1,33 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Start_SDF("boxes.sdf")
+
+length = 1
+width = 1
+height = 1
+
+x_start = 0
+y_start = 0
+z_start = height / 2
+
+rows = 5
+columns = 5
+tower_height = 10
+
+for row in range(rows):
+	for column in range(columns):
+		z = z_start
+		curr_length = length
+		curr_width = width 
+		curr_height = height
+
+		for level in range(tower_height):
+			x = x_start + row * length
+			y = y_start + column * length
+		
+			pyrosim.Send_Cube(name=f"Box_{row}_{column}_{level}", pos=[x,y,z] , size=[curr_length,curr_width,curr_height])
+			z += height
+			curr_length *= 0.9
+			curr_width *= 0.9
+			curr_height *= 0.9
+
 pyrosim.End()
