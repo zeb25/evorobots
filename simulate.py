bf6cbccb04be085edddbdafbebe217e086b5d175
Zoe <zoe.bell@uvm.edu>
2025-02-10 08:27:16 -0500

    Making changes to read sensor values

--- simulate.py
+++ simulate.py
@@ -1,19 +1,26 @@
 import pybullet as p
 import time
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
+import numpy as numpy
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
-
+p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
 p.setGravity(0,0,-9.8,physicsClient)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 
 p.loadSDF("world.sdf")
 
-for i in range(1000):
-	print(f"Step: {i}")
+pyrosim.Prepare_To_Simulate(robotId)
+
+backLegSensorValues = numpy.zeros(10000)
+
+for i in range(10000):
 	p.stepSimulation()
+	backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
 	time.sleep(1/60)
 
 p.disconnect()
+print(backLegSensorValues)
